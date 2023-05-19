202b1ed10253a16c31714f4b8363930cc0bd5566
Peter Street <peter.street@spektrix.com>
2023-05-19 14:04:50 +0100

    Complex tests passed

--- 2015/aoc2015/Day1.cs
+++ 2015/aoc2015/Day1.cs
@@ -4,8 +4,15 @@ public class Day1 : IDay
 {
     public string GetPart1(string input)
     {
-        if (input == ")") return "-1";
-        return "1";
+        int interator = 0;
+
+        foreach (var step in input)
+        {
+            if (step == '(') { interator += 1; }
+            else interator -= 1;
+        }
+
+        return interator.ToString();
     }
 
     public string GetPart2(string input)
