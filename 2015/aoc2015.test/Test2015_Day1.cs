58ae295f9a72b980483bec3e31ba6d6fc0dfcd2a
Peter Street <peter.street@spektrix.com>
2023-05-19 14:07:56 +0100

    Complex tests from suite all pass

--- 2015/aoc2015.test/Test2015_Day1.cs
+++ 2015/aoc2015.test/Test2015_Day1.cs
@@ -7,6 +7,12 @@ public class Test2015_Day1
     [InlineData("(", "1", "One Up should return 1")]
     [InlineData("(())", "0", "Going up and Down should return to zero")]
     [InlineData("()()", "0", "Going up and Down should return to zero")]
+    [InlineData("(((", "3", "Simple steps upward to 3")]
+    [InlineData("(()(()(", "3", "Complex path to 3")]
+    [InlineData("())", "-1", "Going into the Basement")]
+    [InlineData("))(", "-1", "Going into the Basement")]
+    [InlineData(")))", "-3", "Simple steps down to -3")]
+    [InlineData(")())())", "-3", "Complex steps down to -3")]
     public void Part1_ShouldReturnRightAnswer(string input, string expectedOutput, string why)
     {
         var test = new TestDay();
