288993b49c49c1165e75d0a434a2bb8cab8164ed
Peter Street <peter.street@spektrix.com>
2023-05-19 15:16:16 +0100

    Refactor

--- 2015/aoc2015/Day1.cs
+++ 2015/aoc2015/Day1.cs
@@ -2,17 +2,37 @@ namespace aoc2015;
 
 public class Day1 : IDay
 {
+    private int floor = 0;
+
+    private void Start() {
+        floor = 0;
+    }
+
+    private int Step(char direction) {
+        switch (direction) {
+            case '(':
+                floor += 1;
+                break;
+            case ')':
+                floor -= 1;
+                break;
+            default:
+                throw new ArgumentException($"Cannot make a step using {direction}", nameof(direction));
+        }
+
+        return floor;
+    }
+
     public string GetPart1(string input)
     {
-        int interator = 0;
+        Start();
 
         foreach (var step in input)
         {
-            if (step == '(') { interator += 1; }
-            else interator -= 1;
+            Step(step);
         }
 
-        return interator.ToString();
+        return floor.ToString();
     }
 
     public string GetPart2(string input)
