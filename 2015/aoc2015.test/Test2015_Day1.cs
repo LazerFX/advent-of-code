99d05a7bd6d70f44c24b0ac51ba66bda2b13dd5d
Peter Street <peter.street@spektrix.com>
2023-05-19 14:02:39 +0100

    Additional red tests

--- 2015/aoc2015.test/Test2015_Day1.cs
+++ 2015/aoc2015.test/Test2015_Day1.cs
@@ -5,6 +5,8 @@ public class Test2015_Day1
     [Theory]
     [InlineData(")", "-1", "One Down should return -1")]
     [InlineData("(", "1", "One Up should return 1")]
+    [InlineData("(())", "0", "Going up and Down should return to zero")]
+    [InlineData("()()", "0", "Going up and Down should return to zero")]
     public void Part1_ShouldReturnRightAnswer(string input, string expectedOutput, string why)
     {
         var test = new TestDay();
