fc0b11d90760ea42a90861a32ab9cada2fc34bcd
Peter Street <peter.street@spektrix.com>
2023-05-22 15:26:53 +0100

    Simplified each days test run process - would   like to further automate, but lets see   how this goes for now

--- template/TestDay.cs
+++ template/TestDay.cs
@@ -2,43 +2,19 @@ namespace aoc.template;
 
 public class TestDay
 {
-    private IDay? _dayToTest;
-    private string _input = "";
-    private string _expectedOutput = "";
+    private IDay _dayToTest;
 
-    public TestDay WithDay(IDay dayToTest) {
-        _dayToTest = dayToTest;
-        return this;
+    public void TestPart1(DayTestData testData)
+    {
+        _dayToTest.GetPart1(testData.input).Should().Be(testData.expectedOutput, testData.why);
     }
 
-    public TestDay WithInput(string input) {
-        _input = input;
-        return this;
+    public void TestPart2(DayTestData testData)
+    {
+        _dayToTest.GetPart2(testData.input).Should().Be(testData.expectedOutput, testData.why);
     }
 
-    public TestDay WithExpectedOutput(string expectedOutput) {
-        _expectedOutput = expectedOutput;
-        return this;
-    }
-
-    [MemberNotNull(nameof(_dayToTest))]
-    private void ValidateDay() {
-        if (_dayToTest == null) {
-            throw new InvalidDataException($"You need to run {nameof(WithDay)} first");
-        }
-    }
-
-    public void TestPart1(string why) {
-        ValidateDay();
-
-        _dayToTest.GetPart1(_input).Should().Be(_expectedOutput, why);
-    }
-
-    public void TestPart2(string why) {
-        ValidateDay();
-
-        _dayToTest.GetPart2(_input).Should().Be(_expectedOutput, why);
-    }
-
-    public TestDay() {}
+    public TestDay(IDay day) { _dayToTest = day; }
 }
+
+public record DayTestData(string input, string expectedOutput, string why);
